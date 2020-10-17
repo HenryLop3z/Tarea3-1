39d197daa424fa0d689f61d6b7f5cbc367ee7f42
9911sofia <70818875+9911sofia@users.noreply.github.com>
2020-10-16 20:49:20 -0600

    Subo setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="Tarea3",
+    version="0.0.1",
+    author="9911sofia",
+    author_email="9911sofia@gmail.com",
+    description="Tarea3RamirezLeeDonatoLopez",
+    long_description=long_description,
+    url="https://github.com/9911sofia/Tarea3.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        ...],
+)
+    
