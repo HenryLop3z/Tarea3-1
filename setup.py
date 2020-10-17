a55a39e249089cb30640c063db493af3e5e7d84e
9911sofia <70818875+9911sofia@users.noreply.github.com>
2020-10-16 22:02:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,20 @@ setuptools.setup(
     description="Tarea3RamirezLeeDonatoLopez",
     long_description=long_description,
     url="https://github.com/9911sofia/Tarea3.git",
+    packages=['T3'],
     install_requires=['Pillow', 'tabulate', 'playsound', 'argparse'],
+    package_data={  # Archivos extras a los paquetes
+        'T3': ['meca.jpg', 'Texto_ejemplo.txt', 'Hello.mp3'],
+    },
+    package_dir={'T3': 'T3'},  # especifica el directorio de los paquetes
+    python_requires='>=3.3',  # Versión de Python compatible
+    scripts=['T3/lector_texto.py', 'T3/presentador_de_audio.py', 'T3/Presentador_de_imágenes.py'] 
     packages=setuptools.find_packages(),
     classifiers=[
         ...],
 )
+
+    
+   
+   
     
